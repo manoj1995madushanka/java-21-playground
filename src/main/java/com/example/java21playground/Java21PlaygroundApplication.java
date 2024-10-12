e3d772003103c900f8449196f83b9dae1e12df3d
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-12 10:23:49 +0530

    initialize project

---
+++ src/main/java/com/example/java21playground/Java21PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.java21playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21PlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java21PlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java21playground/Java21PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java21playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21PlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
